05a79dcba3934c834e5d22ad032a5141c105932b
mayurhapani <hapanimayur@gmail.com>
2023-10-06 15:59:44 +0530

    06-10-2023/2

--- test/3 strctur exp.c
+++ test/3 strctur exp.c
@@ -3,20 +3,37 @@
 struct Student
 {
     char name[50];
+    int rollno;
     int marks[3];
-} s1[];
+} s1[20];
+
+int subj = 3;
+
+void getelem();
+void display();
 
 int main()
 {
-    int nub_std, subj = 3;
+    int nub_std;
 
     printf("Enter the number of students: ");
     scanf("%d", &nub_std);
 
+    getelem(nub_std);
+
+    display(nub_std);
+
+    return 0;
+}
+void getelem(int nub_std)
+{
     for (int i = 0; i < nub_std; i++)
     {
         printf("Enter the name of student %d: ", i + 1);
         scanf("%s", s1[i].name);
+
+        printf("Enter the roll number of student: ");
+        scanf("%d", &s1[i].rollno);
 
         for (int j = 0; j < subj; j++)
         {
@@ -24,7 +41,9 @@ int main()
             scanf("%d", &s1[i].marks[j]);
         }
     }
-
+}
+void display(int nub_std)
+{
     printf("\nTotal Marks for Each Student:\n");
     for (int i = 0; i < nub_std; i++)
     {
@@ -37,6 +56,4 @@ int main()
 
         printf("%s: %d\n", s1[i].name, totalMarks);
     }
-
-    return 0;
 }
